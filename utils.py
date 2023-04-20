9be62d547c2941e10449e6d4b1c4b82be47c7c24
M0ThyT <Timothyddoerr@gmail.com>
2023-04-20 09:38:40 -0400

    initial files

---
+++ utils.py
@@ -0,0 +1,25 @@
+import pandas as pd
+import numpy as np
+
+def get_api_key(personal = True):
+    """
+    Returns the path to the api key file.
+    Choose between personal and professional api key.
+
+    """
+    base_path = "/Users/tim/Documents/DS projects/Open AI/API Keys/"
+    if personal:
+        path = base_path + "personal.txt"
+    else:
+        path = "css.txt"
+    
+    with open(path, "r") as f:
+        api_key = f.read()
+
+    
+    return api_key
+
+
+
+
+
